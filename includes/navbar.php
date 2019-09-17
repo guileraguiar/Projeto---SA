06b41de796dcdf2c53e8d0e903c1cd45e40f7566
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-09-17 19:33:08 -0300

    redimensionamento

--- includes/navbar.php
+++ includes/navbar.php
@@ -15,7 +15,7 @@
     </div>
   </div>
   <nav class="navbar navbar-dark colortonav" style="opacity:0.7;">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <span class="navbar-toggler-icon"></span>
     </button>
   </nav>
