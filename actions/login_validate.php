0abc1836bfbcd1e771f1003a5e73762e9fd47e8b
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-09-17 21:53:10 -0300

    inserção no banco de dados(login)

--- actions/login_validate.php
+++ actions/login_validate.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
 if((isset($_POST["email"])) && (isset($_POST["pass"]))){
-    $escapeEmail = mysqli_real_escape_string($conn, $_POST['email']);
-    $escapeUsuario = mysqli_real_escape_string($conn, $_POST['pass']);
+    $emailUser = $_POST["email"];
+    $senhaUser = $_POST["pass"];
+    $conexao = mysqli_connect("localhost", "root", "", "db_agonizingVillage");
+    $db = mysql_select_db('db_agonizingVillage');
+    $select_db = "SELECT email FROM usuario WHERE email = '$emailUser'";
+    $select_all = mysql_query($select_db,$conexao);
+    $array = mysql_fetch_array($select_all);
+    
 }else{
     $_SESSION['loginErro'] = "Usuário ou senha Inválido";
     header("Location: ../pages/user/login_page.php");
