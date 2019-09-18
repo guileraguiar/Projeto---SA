57dc9d7ffe488f6a3bc58f8691c451c13f713bb4
felipemachado12 <55164723+felipemachado12@users.noreply.github.com>
2019-09-17 21:18:44 -0300

    insercao no banco de dados

--- actions/register_validate.php
+++ actions/register_validate.php
@@ -1,14 +1,21 @@
+<pre>
 <?php
-$usuario = isset(($_POST["user"])?($_POST["user"]:""));
-$senhaUser = isset(($_POST["pass"])?($_POST["pass"]:""));
-$csenhaUser = isset(($_POST["cpass"])?($_POST["cpass"]:""));
-$emailUser = isset(($_POST["email"])?($_POST["email"]:""));
+$usuario = isset($_POST["user"])?($_POST["user"]):"";
+$senhaUser = isset($_POST["pass"])?($_POST["pass"]):"";
+$csenhaUser = isset($_POST["cpass"])?($_POST["cpass"]):"";
+$emailUser = isset($_POST["email"])?($_POST["email"]):"";
 
+$conexao = mysqli_connect("localhost", "root", "", "db_agonizingVillage");
+$select = mysqli_query($conexao,"SELECT * FROM estudante");
+// $arrayUsers = mysqli_fetch_all($select,MYSQLI_ASSOC);
+// print_r($arrayUsers);
+echo $emailUser;
 if($senhaUser == $csenhaUser){
-    $conexao = mysqli_connect("localhost", "root", "", "db_agonizingVillage");
+    
     $query = mysqli_query($conexao,"INSERT INTO usuario VALUES(DEFAULT,'$usuario','$senhaUser','$emailUser')") or die(mysqli_error($conexao));
 }else{
     echo "<script>alert('Senhas não condizem!');</script>";
+    header("Location:../pages/user/register_page.php");
 }
-
-?>
+mysqli_close($conexao);
+?></pre>
