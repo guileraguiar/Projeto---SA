a40c601766a5629e5ae7818d59dbf49fb3811d3f
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-09-18 22:19:12 -0300

    validação finalizada de login(funcional)

--- actions/login_validate.php
+++ actions/login_validate.php
@@ -1,28 +1,48 @@
 <?php
-session_start();
+    //conexao com o banco de dados 
+    session_start();
 
-    $emailUser = $_POST["email"];
-    $senhaUser = md5($_POST["pass"];
-    $signIN = $_POST["logar"];
-    $conexao = mysqli_connect("localhost", "root", "root", "db_agonizingVillage");
-    $db = mysql_select_db('db_agonizingVillage');
-    
-    if(isset($_POST[$logar])) {
+    $emailUser = isset($_POST["email"])?($_POST["email"]):"";
+    $senhaUser = MD5(isset($_POST["pass"])?($_POST["pass"]):"");
 
-        $query = mysql_query($conexao,"SELECT * FROM usuario WHERE  = 
-        '$usuario' AND senha = '$senhaUser'") or die(mysqli_error($conexao);
+    $local = "localhost";
+    $userRoot = "root";
+    $passRoot = "root";
+    $db_name = "db_agonizingvillage";
+    $conexao = mysqli_connect($local, $userRoot , $passRoot,$db_name) or die (mysqli_error()); 
+?>
 
-      if (mysql_num_rows($verifica)<=0){
-        echo"<script language='javascript' type='text/javascript'>
-        alert('Login e/ou senha incorretos');window.location
-        .href='login.html';</script>";
-        die();
-      }else{
-        setcookie("login",$usuario);
-        header("Location:../../menu.html");
-      }
+<!-- redimensiona a pagina em 5 segunda (5000 milisegundos) caso login correto, ou incorreto-->
+<html>
+    <head>
+        <title>autentication</title>
+        <script type="text/javascript">
+
+        function loginfailed(){
+            setTimeout("window.location='../pages/user/login_page.php'",1000);
+        }
+        </script>
+    </head>
+    <body>
+    </body>
+<?php
+
+    //consulta com o banco 
+        $select = mysqli_query($conexao,"SELECT * FROM usuario WHERE  email = '$emailUser' AND senha = '$senhaUser'") or die(mysqli_error());
+        $rowsBD = mysqli_num_rows($select);
+
+      if (($emailUser != "") && ($senhaUser != "")){
+        $_SESSION["email"] = $_POST["email"];
+        $_SESSION["pass"] = $_POST["pass"];
+        
+    }else{
+        echo "<center>Usuário ou senha incorretos!</center>";
+        echo "<script>loginfailed()</script>";
     }
-    
 
+    header("Location:../menu.php");
+   
+
+    mysqli_close($conexao);
   
 ?>
