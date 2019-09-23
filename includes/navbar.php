886ea126056bde5ac57048959c85a23c191fed96
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2019-09-23 20:56:53 -0300

    criação menu

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>Navbar</title>
     </head>
     <body>
        <!--Navbar -->
@@ -16,22 +16,27 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
     <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">
-          <i class="fab fa-facebook-f"></i> Cadastro
+    <li class="nav-item">
+        <a class="nav-link" href="../../menu.php">
+          <i class="fab fa-facebook-f"></i> Menu
           <span class="sr-only">(current)</span>
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../../pages/user/register_page.php">
-          <i class="fab fa-instagram"></i>Cadastrar-se</a>
+        <a class="nav-link" href="pages/user/register_page.php">
+          <i class="fab fa-facebook-f"></i> Cadastro
+        </a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/user/login_page.php">
+          <i class="fab fa-facebook-f"></i> Login
+        </a>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
           aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user"></i> Profile </a>
+          <i class="fas fa-user"></i> Perfil </a>
         <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-          <a class="dropdown-item" href="#">My account</a>
+          <a class="dropdown-item" href="#">Minha Conta</a>
           <a class="dropdown-item" href="#">Log out</a>
         </div>
       </li>
