1ef08a2374dc15eacdf9ab5fad8315a09c1f775c
felipemachado12 <55164723+felipemachado12@users.noreply.github.com>
2019-10-23 22:45:19 -0300

    correção de inserção de personagem no banco de dados

--- actions/char_validate.php
+++ actions/char_validate.php
@@ -2,44 +2,86 @@
 $nick = isset($_POST["nickname"])?($_POST["nickname"]):"";
 $race = isset($_POST["raca"])?($_POST["raca"]):"";
 $picture = isset($_POST["foto-personagem"])?($_POST["foto-personagem"]):"";
-  // O nome original do foto-personagem no computador do usuário
-  $arqName = $_FILES['foto-personagem']['name'];
-  // O tipo mime do foto-personagem. Um exemplo pode ser "image/gif"
-  $arqType = $_FILES['foto-personagem']['type'];
-  // O tamanho, em bytes, do foto-personagem
-  $arqSize = $_FILES['foto-personagem']['size'];
-  // O nome temporário do foto-personagem, como foi guardado no servidor
-  $arqTemp = $_FILES['foto-personagem']['tmp_name'];
-  // O código de erro associado a este upload de foto-personagem
-  $arqError = $_FILES['foto-personagem']['error'];
 
 
 $local = "localhost";
 $userRoot = "root";
 $passRoot = "root";
 $db_name = "db_agonizingVillage";
-$conexao = mysqli_connect($local, $userRoot , $passRoot,$db_name) or die (mysqli_error());
-    
-//     if($insert){
-//         echo"<script language='javascript' type='text/javascript'>
-//         alert('O seu cadastro foi efetuado com sucesso!!');window.location.
-//         href='../pages/user/login_page.php'</script>";
-//     }else{
-//         echo"<script language='javascript' type='text/javascript'>
-//         alert('Não foi possível efetuar esse cadastro!!'); window.location.href='../pages/user/login_page.php'</script>";
-//     }
-// }
-$tiposPermitidos= array('image/gif','image/png','image/jpeg','image/bmp','image/webp');
+
+switch ($race){
+  case "elfo":
+    $race = 1;
+    $forca = 15;
+    $defesa = 5;
+    $inventario = null;
+    $level = 1;
+    $habilidades = null;
+    $chanceCrit = 0;
+    $dinheiro = 0;
+    $exp = 0;
+    $vida = 100;
+    $energia = 100;
+  break;
+
+  case "orc":
+    $race = 2;
+    $forca = 20;
+    $defesa = 10;
+    $inventario = null;
+    $level = 1;
+    $habilidades = null;
+    $chanceCrit = 0;
+    $dinheiro = 0;
+    $exp = 0;
+    $vida = 300;
+    $energia = 150;
+  break;
+
+  case "mago":
+    $race = 3;
+    $forca = 2;
+    $defesa = 3;
+    $inventario = null;
+    $level = 1;
+    $habilidades = null;
+    $chanceCrit = 0;
+    $dinheiro = 0;
+    $exp = 0;
+    $vida = 100;
+    $energia = 300;
+  break;
+
+  case "humano":
+  //Atributos base da raça humana
+    $race = 4;
+    $forca = 10;
+    $defesa = 3;
+    $inventario = null;
+    $level = 1;
+    $habilidades = null;
+    $chanceCrit = 0;
+    $dinheiro = 0;
+    $exp = 0;
+    $vida = 100;
+    $energia = 100;
+  break;
+}
+echo ($race);
 if ($picture == 0) {
-    $pasta = '../images/personagens/';
-    $upload = move_uploaded_file($arqTemp, $pasta . $arqName);
+    
 
 
 // Aqui você faz a conexão com o banco de dados
+$conexao = mysqli_connect($local, $userRoot , $passRoot,$db_name) or die (mysqli_error());
   // Lista de tipos de arquivos permitidos
-  $tiposPermitidos= array('image/jpeg', 'image/pjpeg', 'image/png');
+  //$tiposPermitidos= array('image/jpg','image/jpeg','image/pjpeg','image/png');
+// Array com as extensões permitidas
+$tiposPermitidos = array('image/jpg', 'image/jpeg', 'image/png');
+
+
   // Tamanho máximo (em bytes)
-  $tamanhoPermitido = 1920 * 1080; // 500 Kb
+  $tamanhoPermitido = 1920 * 1080; 
   // O nome original do arquivo no computador do usuário
   $arqName = $_FILES['foto-personagem']['name'];
   // O tipo mime do arquivo. Um exemplo pode ser "image/gif"
@@ -50,35 +92,32 @@ if ($picture == 0) {
   $arqTemp = $_FILES['foto-personagem']['tmp_name'];
   // O código de erro associado a este upload de arquivo
   $arqError = $_FILES['foto-personagem']['error'];
+
+
   if ($arqError == 0) {
         // Verifica o tipo de arquivo enviado
-    if (array_search($arqType, $tiposPermitidos) === false) {
-      echo 'O tipo de arquivo enviado é inválido!';
-    // Verifica o tamanho do arquivo enviado
-    } else if ($arqSize > $tamanhoPermitido) {
-      echo 'O tamanho do arquivo enviado é maior que o limite!';
-    // Não houveram erros, move o arquivo
+    // Faz a verificação da extensão do arquivo enviado
+// $extensao = strrchr($_FILES['foto-personagem']['name'],'.');
+
+// Faz a validação do arquivo enviado
+if (array_search($arqType, $tiposPermitidos) === false) {
+  echo 'O tipo de arquivo enviado é inválido!';
+// Não houveram erros, move o arquivo
     } else {
-      // Escapa os caracteres protegidos do MySQL (para o nome do usuário)
-      $nomeMySQL = mysqli_real_escape_string($conexao,$_POST['nickname']);
-      $upload = move_uploaded_file($arqTemp, $pasta);
+      $pasta = "../images/personagens/";  
+      $upload = move_uploaded_file($arqTemp, $pasta.$arqName);
+      $foto = $_FILES["foto-personagem"]['name'];
+      
       // Verifica se o arquivo foi movido com sucesso
       if ($upload == true) {
         // Cria uma query MySQL
-        $query = mysqli_query($conexao, "INSERT INTO characters (c_nickname,c_race,c_picture) VALUES ('$nick','$race','$picture')");
-        // Executa a consulta
-        if ($query == true) {
-                    echo 'Usuário inserido com sucesso!';
-                }
+        $query = mysqli_query($conexao, "INSERT INTO characters (c_nickname,fk_race,c_experience,c_life,c_energy,c_strenght,c_defense,c_inventory,c_level,c_ability,c_crit_chance,c_money,c_picture) VALUES ('$nick',DEFAULT,'$exp','$vida','$energia','$forca',$defesa,'$inventario','$level','$habilidades','$chanceCrit','$dinheiro','$foto')");
+        print_r ($query);
+        echo 'Usuário inserido com sucesso!';
+        }else {
+          echo 'Ocorreu algum erro com o upload, por favor tente novamente!';
       }
     }
-  } else {
-    echo 'Ocorreu algum erro com o upload, por favor tente novamente!';
-  }
-  }
-
-
-
-
-
+  } 
+}
 ?>
