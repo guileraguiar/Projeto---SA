2bc93e3b8f93481f3edc23e78bc53b788c93bf40
felipemachado12 <55164723+felipemachado12@users.noreply.github.com>
2019-11-11 19:06:32 -0300

    fixed view character

--- pages/user/view/user_profile.php
+++ pages/user/view/user_profile.php
@@ -13,18 +13,22 @@ session_start();
         require_once "../../../bd/connection_bd.php";
           $userSession = $_SESSION['user'];
                    
-          if(isset($userSession)){ 
-          
+          if(isset($userSession)): 
+            $slChar = mysqli_query($conexao,"SELECT * FROM characters,race WHERE users_id_user = $userSession[id_user]");
             $select = mysqli_query($conexao,"SELECT u_user FROM users WHERE u_user = ".$userSession["u_user"]."");
             $selectEmail = mysqli_query($conexao,"SELECT u_email FROM users WHERE u_user = ".$userSession["u_email"]."");
             $selectEmail = mysqli_query($conexao,"SELECT id_user FROM users WHERE u_user = ".$userSession["id_user"]."");
             $selectEmail = mysqli_query($conexao,"SELECT u_pass FROM users WHERE u_user = ".$userSession["u_pass"]."");
+            $arrayChar= mysqli_fetch_assoc($slChar);
+           
+            
+            
         
           ?> 
         <br>
-        <div class="container fundobranco" style="width:900px; height:420px; margin-top:20px;">
+        <div class="container fundobranco" style="width:auto; height:auto; margin-top:20px;">
             <div class="row" style="padding:20px;">
-                <div class="col-sm-3"><img src="../../../images/personagens/character1.gif" style="margin-left:30px; width:100px;" class="rounded float-left profileImage" alt="..."> 
+                <div class="col-sm-3"><img src="http://localhost/SteelFreak/images/personagens/<?php echo $arrayChar["c_picture"]; ?>" style='margin-left:5px; width:200px;' class='rounded float-left profileImage' alt='...'> 
                 </div>
                 <div class="col-sm-9">
                     <h4 class="minhaconta">Minha conta</h4>
@@ -37,7 +41,7 @@ session_start();
                     <h4 class="profileBTxt">|Usuário:</h4>
                 </div>
                 <div class="col">
-                    <p class="profileTxt" ><?php echo $userSession["u_user"] ?></p>
+                    <p class="profileTxt"><?php echo $userSession["u_user"] ?></p>
                 </div>
             </div>
             <div class="row no-gutters">
@@ -59,19 +63,19 @@ session_start();
             <hr>
             <!-- Dados do personagem (ainda não está funcionando) //-->
             <div class="row no-gutters">
-            <div class="col">
-                <h4 class="profileBTxt" style="margin-left:400px;">|Apelido: </h4>
-            </div>
-            <div class="col">
-                <p class="profileTxt" ></p>
-            </div>
+                <div class="col">
+                    <h4 class="profileBTxt">|Apelido: </h4>
+                </div>
+                <div class="col">
+                    <p class="profileTxt" ><?php echo $arrayChar["c_nickname"] ?></p>
+                </div>
         </div>
         <div class="row no-gutters">
             <div class="col">
-                <h4 class="profileBTxt" style="margin-left:400px;">|Raça: </h4>
+                <h4 class="profileBTxt">|Raça: </h4>
             </div>
             <div class="col">
-                <p class="profileTxt" ></p>
+                <p class="profileTxt" ><?php echo $arrayChar["r_name"] ?></p>
             </div>
         </div>
         </div>
