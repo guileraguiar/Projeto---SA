a0f1dd4f0c8b40002f712be3472e6110fd80bb58
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-11-11 20:56:59 -0300

    batalha
    construção da pagina de batalha, visual

---
+++ battle.php
@@ -0,0 +1,48 @@
+<?php
+include 'includes/header.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <body class="fadeIn" style="background-size: 100%;background-image:; ">
+    <img src="images/TITULO.png"class="mx-auto d-block titulo"  alt=""> 
+        <div class="container fundobranco" style="border-radius:3px;">
+            <div class="row">
+                <div class="col-sm" style="margin:50px;">
+                    <div class="card" style="width: 15rem;">
+                        <img class="card-img-top" src="images/logo/kuriboh.png" alt="Imagem de capa do card">
+                        <ul>
+                            <li>
+                                <span>ATK:</span>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <span>DEF:</span>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <span>:</span>
+                            </li>
+                        </ul>
+                        <div class="card-body">
+                            <h5 class="card-title">Título do card</h5>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"><button  class="btn btn-primary" type="" value="">habilidade1</button></li>
+                                <li class="list-group-item"><button  class="btn btn-primary" type="" value="">habilidade2</button></li>
+                                <li class="list-group-item"><button  class="btn btn-primary" type="" value="">habilidade3</button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm">
+                Uma de três colunas
+                </div>
+                <div class="col-sm">
+                Uma de três colunas
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
