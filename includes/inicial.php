c080c24548bc8f14f8d5b126e6013dda71ca6bbf
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-11-14 19:46:56 -0300

    ajuste

---
+++ includes/inicial.php
@@ -0,0 +1,18 @@
+<body class="fadeIn" style="background-color:#302140;">
+    <div class="container">
+        <img src="images/TITULO.png" class="mx-auto d-block imgIndex" alt="">
+        <center>
+            <h1 class="letraMenuIndex">Powered By SteelFreak Inc.</h1>
+        </center>
+        <center>
+            <h1 class="phrase">"Make the Magic Happen."</h1>
+        </center>
+        <br>
+        <center><img style="width:80px;" src="images/wand.png" alt=""></center>
+        <br>
+        <br>
+        <form action="http://localhost/SteelFreak/public/index.php">
+            <center><button type="submit" class="btn mx-auto d-block">Entrar</button></center>
+        </form>
+    </div>
+</body>
