47fec5a9ab500860020b1c8a594f5c7ddbbc8163
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-11-20 16:31:26 -0300

    coisas inuteis

--- pages/user/view/user_profile.php
+++ pages/user/view/user_profile.php
@@ -2,12 +2,7 @@
   
     $userSession = $_SESSION['user'];
     if (isset($userSession)) :
-        $slChar = mysqli_query($conexao, "SELECT * FROM characters,race WHERE users_id_user = $userSession[id_user]");
-        $select = mysqli_query($conexao, "SELECT u_user FROM users WHERE u_user = " . $userSession["u_user"] . "");
-        $selectEmail = mysqli_query($conexao, "SELECT u_email FROM users WHERE u_user = " . $userSession["u_email"] . "");
-        $selectEmail = mysqli_query($conexao, "SELECT id_user FROM users WHERE u_user = " . $userSession["id_user"] . "");
-        $selectEmail = mysqli_query($conexao, "SELECT u_pass FROM users WHERE u_user = " . $userSession["u_pass"] . "");
-        $arrayChar = mysqli_fetch_assoc($slChar);
+        $select = mysqli_query($conexao, "SELECT * FROM users WHERE u_user = " . $userSession["u_user"] . "");
         ?>
         <br>
         <div class="container fundobranco w-50 p-12" style="width:auto; height:auto; margin-top:20px;">
