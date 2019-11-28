8cdda34dd7c1a2290b1bc184acac6dd815aee443
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-11-28 16:11:11 -0300

    Update menu.php

--- pages/menu.php
+++ pages/menu.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user'])) :
     <img src="images/logo3.png"   style=" margin-bottom:20px; margin-top:20px; "class="mx-auto d-block" alt="" height="300" width="300">
     <div class="container fundoBackground" style="width:750px; margin-bottom:50px;">
         <center style="margin-top:50px;">
-            <h1>Bem Vindo!</h1>
+            <h1>Bem Vindo Visitante!</h1>
         </center>
         <center>
             <div class="containerBranco" style="width:600px; margin-top:50px; margin-bottom:50px;background-color:#80808099;">
@@ -31,7 +31,6 @@ if (!isset($_SESSION['user'])) :
                     <h4 class="h44">Dúvidas sobre o funcionamento do jogo? leia o guia com todos os detalhes!</h4>
                 </center>
                 <a href="index.php?pagina=x" class="btn btn-dark">Introdução</a>
-                </form>
                 <br>
             </div>
     </div>
@@ -43,24 +42,16 @@ if (!isset($_SESSION['user'])) :
     </div>
 <?php
 endif;
+if(isset($_SESSION['user'])):
 ?>
-<!-- <center>
-                <h5>Você Sabia?</h5>
-            </center>
-            <center>
-                <p class="fonteLabel">O jogo Agonizing Village 2 foi inspirado em The Witcher 3, tanto com o
-                    nome do personagem Geraldo do Rio(Geralt of Rivia), como também, o seu ambiente, totalmente inspirado no game.
-                </p>
-            </center>
-            <br>
-            <br>
-            <br>
-            <center>
-                <h5>Você Sabia?</h5>
-            </center>
-            <center>
-                <p class="fonteLabel">A logo de Agonizing Village 2 é uma referência ao seu jogo
-                    anterior (Agonizing Village), referindo-se ao lobo Fenrir, na qual Fenrir estava em apuros, e você poderia decidir se iria salva-lo, ou deixa-lo sozinho
-                    em seu sofrimento...
-                </p><img style="width:300px;" src="images/logoav2.png" alt="">
-            </center> -->
+<img src="images/logo3.png"   style=" margin-bottom:20px; margin-top:20px; "class="mx-auto d-block" alt="" height="300" width="300">
+    <div class="container fundoBackground" style="width:750px; margin-bottom:50px;">
+        <center style="margin-top:50px;">
+            <h1>Bem Vindo <?php echo $userSession['u_user'];?>!</h1>
+        </center>
+        <center>
+            <div class="containerBranco" style="width:600px; margin-top:50px; margin-bottom:50px;background-color:#80808099;">
+              
+            </div>
+    </div>
+<?php endif ?>
