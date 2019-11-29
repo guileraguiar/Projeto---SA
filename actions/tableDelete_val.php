d60333ab52df5463ac8a10e8320f48b00aacbed0
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-11-28 22:17:38 -0300

    table

--- actions/tableDelete_val.php
+++ actions/tableDelete_val.php
@@ -7,5 +7,12 @@ if (isset($codigo)) {
 
     $select = mysqli_query($conexao, "SELECT u_type FROM users");
     $deleteUsers = mysqli_query($conexao, "DELETE FROM users WHERE id_user='" . $codigo . "' AND u_type <> 2");
+    if(mysqli_num_rows($deleteUsers)==1){
+        $msgDel = 1;
+        header("Location:../public/index.php?pagina=viewUsers&msgDel='. $msgDel.'");
+    }else{
+        $msgDel = 2;
+        header("Location:../public/index.php?pagina=viewUsers&msgDel='. $msgDel.'");
+    }
 }
 mysqli_close($conexao);
