ae172e5d5d8fddbfc4a85e378b88595841d4cc20
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-12-02 14:51:58 -0300

    botão voltar

--- pages/user/wiki_pageThree.php
+++ pages/user/wiki_pageThree.php
@@ -14,16 +14,13 @@
 </section>
 <div class="container" style="text-align:center;">
     <div class="fundobranco2 profileBTxt page3">
-    <center><h3 style="color:yellow;">ATRIBUTOS DE GERALDO</h3></center>
-    <p class="borderText textPosition" >Os atributos de Geraldo do rio são: Dano de ataque, vida, e proteção, que poderão ser evoluídos ao derrotar inimigos, conversar com NPC's e descobrindo baús secretos. </p> 
-    <div class="row p-5">
-            <div class="col">
-                <a href="?pagina=pageWiki2" class="proximo" style="color:yellow;">Voltar</a>
-            </div>
-            <div class="col">
-                <a href="?pagina=pageWiki3" class="proximo" style="color:yellow;">Próximo</a>
-            </div>
-        </div>  
+        <center>
+            <h3 style="color:yellow;">ATRIBUTOS DE GERALDO</h3>
+        </center>
+        <p class="borderText textPosition">Os atributos de Geraldo do rio são: Dano de ataque, vida, e proteção, que poderão ser evoluídos ao derrotar inimigos, conversar com NPC's e descobrindo baús secretos. </p>
+        <div class=" p-5">
+            <center><a href="?pagina=pageWiki2" class="proximo" style="color:yellow;">Voltar</a></center>
+        </div>
     </div>
 </div>
-
+</div>
