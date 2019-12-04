600e5f5679d78a9906145a70681293949d81b509
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2019-12-03 21:02:07 -0300

    Update TheGame.php

--- pages/TheGame.php
+++ pages/TheGame.php
@@ -17,62 +17,59 @@ if (isset($_SESSION['user'])) :
     ?>
 
 <center>
-    <img src="images/logo3.png" style=" margin-bottom:20px; margin-top:20px; " class="mx-auto d-block" alt=""height="300" width="300">
-    <div class="container fundoBackground fundobranco" style="width:1200; margin-bottom:50px;">
-
-            <h1 class="gameName">JOGOS</h1>
+        <div id="carouselExampleIndicators" href="#carousel-1" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
                 
-                <div id="carouselExampleIndicators" href="#carousel-1" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            
-                            <div class="carousel-item active">
-                            <img class="d-block w-75" src="images/pageGames/AV1/LogoAV.png" alt="AV1">
-                            <div class="carousel-caption d-none d-md-block">
-                            </div>
-                            </div>
+                <div class="carousel-item active">
+                <img class="d-block w-25" src="images/pageGames/AV1/LogoAV.png" alt="AV1">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+                </div>
 
-                            <div class="carousel-item">
-                            <img class="d-block w-75" src="images/pageGames/AV2/LogoAV2.png" alt="AV2">
-                            <div class="carousel-caption d-none d-md-block">
-                            </div>
-                            </div>
+                <div class="carousel-item">
+                <img class="d-block w-25" src="images/pageGames/AV2/LogoAV2.png" alt="AV2">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+                </div>
 
-                            <div class="carousel-item">
-                            <img class="d-block w-75" src="images/logoav3.png" alt="AV3">                            
-                            <div class="carousel-caption d-none d-md-block">>
-                            </div>                  
-                            </div>
+                <div class="carousel-item">
+                <img class="d-block w-25" src="images/logoav3.png" alt="AV3">                            
+                <div class="carousel-caption d-none d-md-block">>
+                </div>                  
+                </div>
 
 
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-
-                        <p class="sinopse">Baixar <a href="?pagina=AV1">Agonizing Village 1</a></p>
-                        <a href="https://drive.google.com/file/d/1VjjR90bn697hAvDxT2nKDU9pI1igpk2t/view?usp=sharing"
-                         style="margin-bottom:50px" target="_blank" class="btn btn-light mx-auto d-block fonteLabel">Baixar!</a>
-
-                        <p class="sinopse">Baixar <a href="?pagina=AV2">Agonizing Village 2</a></p>
-                        <a href="https://drive.google.com/file/d/1pxB1Gh4hM3oHxGoi75yHR7POxfzORBiX/view?usp=sharing"
-                        style="margin-bottom:50px" target="_blank" class="btn btn-light mx-auto d-block fonteLabel">Baixar!</a>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
     
-                        <p class="sinopse">Jogar Agonizing Village 3</p>
-                        <a href="https://drive.google.com/file/d/1VjjR90bn697hAvDxT2nKDU9pI1igpk2t/view?usp=sharing"
-                        style="margin-bottom:50px" target="_blank" class="btn btn-light mx-auto d-block fonteLabel">Jogar!</a>
-                    
-                    </div>
+        <div class="container fundoBackground fundobranco" style="width:1200; margin-top:50px; margin-bottom:50px;">
+
+            <p class="sinopse">Baixar <a href="?pagina=AV1">Agonizing Village 1</a></p>
+            <a href="https://drive.google.com/file/d/1VjjR90bn697hAvDxT2nKDU9pI1igpk2t/view?usp=sharing"
+                style="margin-bottom:50px" target="_blank" class="btn btn-light mx-auto d-block fonteLabel">Baixar!</a>
+
+            <p class="sinopse">Baixar <a href="?pagina=AV2">Agonizing Village 2</a></p>
+            <a href="https://drive.google.com/file/d/1pxB1Gh4hM3oHxGoi75yHR7POxfzORBiX/view?usp=sharing"
+            style="margin-bottom:50px" target="_blank" class="btn btn-light mx-auto d-block fonteLabel">Baixar!</a>
+
+            <p class="sinopse">Jogar Agonizing Village 3</p>
+            <a href="https://drive.google.com/file/d/1VjjR90bn697hAvDxT2nKDU9pI1igpk2t/view?usp=sharing"
+            style="margin-bottom:50px" target="_blank" class="btn btn-light mx-auto d-block fonteLabel">Jogar!</a>
+        
+        </div>
 </center>
     </div>
 <?php endif ?>
