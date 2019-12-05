e2c8c52b6a62cfe0ad40a2378fefd021a0dcf65e
Artur Casagrande Raupp <55216242+arturaupp4422@users.noreply.github.com>
2019-12-05 02:10:01 -0300

    .

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bem Vindo</title>
+    <!-- favicon -->
+    <link rel = "shortcut icon" type = "imagem/x-icon" href = "images/logo.png"/>
     <!-- CSS //-->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/imagem.css">
